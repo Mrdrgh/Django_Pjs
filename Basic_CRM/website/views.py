2b17351ad0202dbccd871298b3fe5899ef332c17
Darghal M <mrdrgh2003@gmail.com>
2024-08-01 00:15:45 +0100

    merging

--- Basic_CRM/website/views.py
+++ Basic_CRM/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(req):
@@ -25,4 +26,29 @@ def logout_user(req):
     messages.success(req, "Logged out")
     return render(req, "home.html")
 
+def register_user(req):
+    # new_user = User(username="")
+    # messages.success(req, "Registration succesful")
+    if req.method == "POST":
+        username = req.POST.get("username")
+        email = req.POST.get("email")
+        password = req.POST.get("password")
+        password_conf = req.POST.get("confirm_password")
+        
+        new_user = User(
+            username=username,
+            email=email,
+        )
+        if User.objects.filter(username=username).exists():
+            messages.success(req, "user already exists")
+            return render(req, "register.html")
+        elif password_conf != password:
+            messages.success(req, "password not confirmed correctly")
+            return render(req, "register.html")
+        else:
+            new_user.set_password(raw_password=password)
+            new_user.save()
+            return redirect('home')
+    else: 
+        return render(req, "register.html")
 
