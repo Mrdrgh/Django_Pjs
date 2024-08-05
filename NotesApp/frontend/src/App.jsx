07adec8717043d64928a4ad6d2272eba47394987
Darghal M <mrdrgh2003@gmail.com>
2024-08-05 20:39:21 +0100

    added Axios interceptor

--- NotesApp/frontend/src/App.jsx
+++ NotesApp/frontend/src/App.jsx
@@ -1,9 +1,45 @@
 import React from "react"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Register from "./pages/register"
+import NotFound from "./pages/notFount"
+import Home from "./pages/home"
+import Notes from "./pages/notes"
+import Login from "./pages/login"
+import ProtectedRoute from "./components/protectedRoute"
 
+function Logout() {
+    localStorage.clear();
+    return (<Navigate to="/login"/>);
+}
+
+function RegisterLogout() {
+    localStorage.clear()
+    return <Register />
+}
 function App() {
     return (
         <>
-            
+            <BrowserRouter>
+                <Routes>
+                    <Route 
+                        path="/"
+                        Component={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route 
+                        path="/api/user/register"
+                        Component={
+                            <RegisterLogout />
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter> 
         </>
     )
 }
