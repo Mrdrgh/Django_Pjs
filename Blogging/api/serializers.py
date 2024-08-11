2ede8c7713e2e3558376eeff06b981b2a1537bcb
Darghal M <mrdrgh2003@gmail.com>
2024-08-11 14:35:41 +0100

    added Blog class and serialized user class

---
+++ Blogging/api/serializers.py
@@ -0,0 +1,19 @@
+from  rest_framework import serializers
+from .models import Blog
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', "username", "password"]
+    
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
--- Blogging/api/views.py
+++ Blogging/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import UserSerializer, BlogSerializer
+from .models import Blog
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
+
+class BlogListCreate(generics.ListCreateAPIView):
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return 
+
